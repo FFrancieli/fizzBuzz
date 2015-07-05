f554c62c2e6ca72ddcbd806208feb05baa041f84
Francieli Viane <francielifrv@gmail.com>
2015-07-05 18:42:40 -0300

    Tests working for number 3

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -8,5 +8,9 @@ describe("Calculates fizzbuzz", function(){
 		expect(fizzBuzz(4)).toBe(4);
 		expect(fizzBuzz(7)).toBe(7);
 		expect(fizzBuzz(201)).toBe(201);
-	})
+	});
+
+	it("returns Fizz word when input is 3", function(){
+		expect(fizzBuzz(3)).toBe(3)
+	});
 });
